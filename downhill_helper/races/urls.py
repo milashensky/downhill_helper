ce0d4e1a96d79246d188b21094e21964e8477131
Alexandr Milashensky <milashensky@gmail.com>
2022-06-05 20:56:03 +0300

    consistency

--- downhill_helper/races/urls.py
+++ downhill_helper/races/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from races.api import RaceApi, QualificationApi, BracketsApi
 
+
 urlpatterns = [
     path('api/race/<str:race_slug>', RaceApi.as_view(), name='race_api'),
     path('api/race/<str:race_slug>/qualification', QualificationApi.as_view(), name='qualification_api'),
